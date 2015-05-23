48eb34318aa58c086544c0236b284779a02e9438
JTCode <jtcode@seznam.cz>
2015-05-23 20:02:55 +0200

    system property list

--- src/cz/gjtcode/testrepo/core/Main.java
+++ src/cz/gjtcode/testrepo/core/Main.java
@@ -1,8 +1,22 @@
 package cz.gjtcode.testrepo.core;
 
+import java.nio.charset.Charset;
+import java.util.Properties;
+import java.util.Set;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("System properties: ");
+		final Properties properties = System.getProperties();
+		final Set<Object> propertyKeySet = properties.keySet();
+		for (Object propertyKey : propertyKeySet) {
+			final Object propertyValue = properties.get(propertyKey);
+			System.out.println(String.format(" - %s: %s", propertyKey, propertyValue));
+		}
+		System.out.println("Next info: ");
+		final Charset defaultCharset = Charset.defaultCharset();
+		System.out.println(String.format(" - DefaultCharset: %s", defaultCharset.displayName()));
 	}
 }
