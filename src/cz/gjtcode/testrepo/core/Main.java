68cab4f312df0a8f88e4b2321665e109c1605bd6
JTCode <jtcode@seznam.cz>
2015-05-23 19:33:42 +0200

    byte array test decoding

--- src/cz/gjtcode/testrepo/core/Main.java
+++ src/cz/gjtcode/testrepo/core/Main.java
@@ -1,8 +1,18 @@
 package cz.gjtcode.testrepo.core;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("App - START");
+		final byte[] testBytes = {'a', 'b', 'c'};
+		final String testStrFromBytes = new String(testBytes, StandardCharsets.UTF_8);
+		System.out.println("TestBytes: " + Arrays.toString(testBytes));
+		System.out.println("TestStrFromBytes: " + testStrFromBytes);
+		System.out.println("App - END");
 	}
+
 }
